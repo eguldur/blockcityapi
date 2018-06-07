c6665b1850bdfc34fe94bd8bd5fb2fe24e22b737
aslanmehmet <m.aslann35@gmail.com>
2018-06-07 17:33:09 +0300

    20180607
    update version 1.2

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/api', verifyToken);
 
 //VERSION
-var version = "1.1";
+var version = "1.2";
 
 function intervalFunc(){
  Match.find({}, function(err, matches){
